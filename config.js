c689b9dafa43e41530ad5125ebdd30eedf50662b
tat2ddaddy <44217499+tat2ddaddy@users.noreply.github.com>
2020-09-06 11:30:33 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 export default {
     bucket: {
-      slug: process.env.COSMIC_BUCKET || 'ffdcbd10-145a-11ea-b00e-a3bdfb218e58',
-      read_key: process.env.COSMIC_READ_KEY || 'ICEOPViWhe1cc7qyOXapfUI2l7jwTO8FPIdQg8yagMjkCpi9T7',
+      slug: process.env.COSMIC_BUCKET || 'hades-active',
+      read_key: process.env.COSMIC_READ_KEY || 'b4VULeNOl5JpaCiGbREddH4DptzNjOvinIqyEcOoK4hUPGtEaU',
       write_key: process.env.COSMIC_WRITE_KEY || '',
       port: process.env.PORT || '',
-      snipcart_api_key: process.env.SNIPCART_KEY || 'OTMwZGE2NDQtZTY5NC00Zjc2LWI2M2QtNTEwNWI4NDNjNGNlNjM3MTA1NTk4NDk1OTE1NDQx'
+      snipcart_api_key: process.env.SNIPCART_KEY || 'YTQ0MTBhZmMtZGMxOC00MDFhLThhOTgtMjBhNDNkMWJjYzZiNjM3MzUwMDgzMjQxODcxMDcw'
     }
   }
