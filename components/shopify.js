7ef4ee5131927a7bcc05739813b1dee69d85dfde
Tyler Huff <tat2d.daddy@gmail.com>
2021-01-31 18:53:22 -0700

    push to vercel

--- components/shopify.js
+++ components/shopify.js
@@ -1,7 +1,7 @@
 import useShopifyGraphQL from "../utils/shopify.products";
 import {useEffect} from "react";
 
-
+// blah blah blah
 
 // This function gets called at build time on server-side.
 // It won't be called on client-side, so you can even do
